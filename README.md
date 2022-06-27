ca2c39fab9ea460fb593f1dcf7a0a047b05d6353
patiname <patiname@naver.com>
2022-06-27 09:52:05 +0900

    get movies

--- README.md
+++ README.md
@@ -6,10 +6,19 @@
 - [x] Router setup
 - [x] Create Components
 - [x] Helmet
-- [ ] Get Api
-- [ ] Use Effect
+- [x] Get Api
+
+## Main page
+
+- [x] Use Effect
 - [ ] try catch
 - [ ] Header Event
 - [ ] Slide
-- [ ] Detail Page
+
+## Detail page
+
 - [ ] Video
+
+## Search page
+
+- [ ] Css Grid
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,15 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { movieApi } from "../../../api";
 
 export const Home = () => {
+  const [playing, setPlaying] = useState();
+  const [rated, setRated] = useState();
+  const [upComming, setUpComming] = useState();
+
   useEffect(() => {
     const movieData = async () => {
-      const {
-        data: { results },
-      } = await movieApi.topRated();
+      try {
+        const {
+          data: { results: playingData },
+        } = await movieApi.nowPlaying();
+        // =>비구조화 할당 이용시 변수명 변경할땐
+        // 변수명:변경할명
+        setPlaying(playingData);
+
+        const {
+          data: { results: ratedData },
+        } = await movieApi.topRated();
+        setRated(ratedData);
+
+        const {
+          data: { results: upCommingData },
+        } = await movieApi.upComming();
+        setUpComming(upCommingData);
+      } catch (error) {
+        console.log(error);
+      }
     };
     movieData();
   }, []);
+
+  console.log("현재상영 영화:", playing);
+  console.log("인기 영화:", rated);
+  console.log("개봉예정 영화:", upComming);
 
   return <div>Home</div>;
 };
