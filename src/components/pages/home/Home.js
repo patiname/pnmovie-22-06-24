06394a1d9a043c991a96b0c5592e29d785adfb87
patiname <patiname@naver.com>
2022-06-27 11:36:29 +0900

    banner end

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,33 +1,8 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import { movieApi } from "../../../api";
-import { mainStyle } from "../../../styles/globalStyle";
+import { movieNum } from "../../../constants/constant";
 import { Loading } from "../../Loading";
-
-const MainBanner = styled.section`
-  height: 80vh;
-  background-color: gray;
-  padding: ${mainStyle.padding};
-  padding-top: 250px;
-`;
-
-const Title = styled.div`
-  max-width: 650px;
-  width: 100%;
-  font-size: 80px;
-  font-weight: 700;
-  line-height: 6rem;
-`;
-
-const Desc = styled.div`
-  max-width: 700px;
-  width: 100%;
-  font-size: 20px;
-  margin-top: 20px;
-  line-height: 2rem;
-  opacity: 0.9;
-  font-weight: 300;
-`;
+import { MainBanner } from "./MainBanner";
 
 export const Home = () => {
   const [playing, setPlaying] = useState();
@@ -55,7 +30,7 @@ export const Home = () => {
         } = await movieApi.upComming();
         setUpComming(upCommingData);
 
-        // setLoading(false);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -72,18 +47,7 @@ export const Home = () => {
       {loading ? (
         <Loading />
       ) : (
-        <>
-          {playing && (
-            <MainBanner
-              style={{
-                background: `url(https://image.tmdb.org/t/p/original/${playing[1].backdrop_path}) no-repeat center / cover`,
-              }}
-            >
-              <Title>{playing[1].title}</Title>
-              <Desc>{playing[1].overview.slice(0, 100) + "..."}</Desc>
-            </MainBanner>
-          )}
-        </>
+        <>{playing && <MainBanner playData={playing[movieNum]} />}</>
       )}
     </div>
   );
