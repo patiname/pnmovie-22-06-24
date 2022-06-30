04cfdae3d41572d19a8f4b06638da5c191d094d9
patiname <patiname@naver.com>
2022-06-30 11:38:13 +0900

    detail-2

--- src/components/pages/detail/Detail.js
+++ src/components/pages/detail/Detail.js
@@ -1,6 +1,59 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { movieApi } from "../../../api";
+import { imgUrl } from "../../../constants/constant";
+import { Container } from "../../Container";
+import { Loading } from "../../Loading";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 100px;
+`;
+
+const Con = styled.div`
+  width: 48%;
+  &:nth-child(1) {
+    height: 80vh;
+  }
+`;
+
+const Title = styled.h3`
+  font-size: 60px;
+  font-weight: 700;
+  margin-bottom: 40px;
+`;
+
+const Release = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Genres = styled.ul`
+  font-size: 20px;
+  font-weight: 600;
+  li {
+    list-style: disc;
+    margin-bottom: 5px;
+  }
+  margin: 20px 0 0 25px;
+`;
+
+const Runtime = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+`;
+
+const Desc = styled.p`
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 2.2rem;
+  margin-top: 30px;
+  opacity: 0.8;
+  letter-spacing: 0.5px;
+`;
 
 export const Detail = () => {
   const [movieData, setMovieData] = useState();
@@ -18,5 +71,40 @@ export const Detail = () => {
   }, []);
 
   console.log(movieData);
-  return <div>Detail</div>;
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {movieData && (
+            <Container>
+              <Wrap>
+                <Con
+                  style={{
+                    background: `url(${
+                      movieData.backdrop_path
+                        ? `${imgUrl}${movieData.backdrop_path}`
+                        : "https://tohoku365.com/desk/wp/wp-content/themes/dp-voyageur/img/post_thumbnail/noimage.png"
+                    }) no-repeat center / cover`,
+                  }}
+                />
+                <Con>
+                  <Title>{movieData.title}</Title>
+                  <Release>개봉일: {movieData.release_date}</Release>
+                  <Runtime>{movieData.runtime} 분</Runtime>
+                  <Genres>
+                    {movieData.genres.map((genre) => (
+                      <li key={genre.id}>{genre.name}</li>
+                    ))}
+                  </Genres>
+                  <Desc>{movieData.overview}</Desc>
+                </Con>
+              </Wrap>
+            </Container>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
