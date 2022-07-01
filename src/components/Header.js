a72c125e1fdb1c59207da6e7b130323ae1d4873e
patiname <patiname@naver.com>
2022-07-01 11:29:31 +0900

    mo menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -25,6 +27,8 @@ const SHeader = styled.div`
 const Logo = styled.h3`
   font-size: 28px;
   font-weight: 800;
+  position: relative;
+  z-index: 10;
   a {
     color: ${mainStyle.mainColor};
   }
@@ -37,6 +41,9 @@ const MenuWrap = styled.ul`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
 `;
 
 const Menu = styled.li`
@@ -48,8 +55,45 @@ const Menu = styled.li`
   }
 `;
 
+const MoMenu = styled.div``;
+
+const MoMenuWrap = styled.ul`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: ${(props) => props.leftResult};
+  background: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(3px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: 0.5s;
+  li {
+    font-size: 50px;
+    font-weight: 700;
+    margin-bottom: 100px;
+    &:nth-child(1) {
+      font-size: 24px;
+    }
+  }
+`;
+
+const MenuBtn = styled.div`
+  font-size: 24px;
+`;
+
+const CloseBtn = styled.li`
+  position: absolute;
+  top: 27px;
+  right: 20px;
+  z-index: 10;
+`;
+
 export const Header = () => {
   const [bg, setBg] = useState("transparent");
+  const [left, setLeft] = useState("100%");
 
   const handleScroll = () => {
     const sct = window.pageYOffset;
@@ -76,6 +120,26 @@ export const Header = () => {
           <Link to={"/search"}>Search</Link>
         </Menu>
       </MenuWrap>
+
+      <MoMenu>
+        <MenuBtn onClick={() => setLeft(0)}>
+          <FontAwesomeIcon icon={faBars} />
+        </MenuBtn>
+
+        <MoMenuWrap leftResult={left}>
+          <CloseBtn onClick={() => setLeft("100%")}>
+            <FontAwesomeIcon icon={faClose} />
+          </CloseBtn>
+          <li>
+            <Link Link to={"/"}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to={"/search"}>Search</Link>
+          </li>
+        </MoMenuWrap>
+      </MoMenu>
     </SHeader>
   );
 };
