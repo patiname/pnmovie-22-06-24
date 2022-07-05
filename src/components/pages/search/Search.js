b0e61157582f58255f78df3845c21ab7e5428372
patiname <patiname@naver.com>
2022-07-05 11:26:30 +0900

    movie detail -2

--- src/components/pages/search/Search.js
+++ src/components/pages/search/Search.js
@@ -54,6 +54,7 @@ export const Search = () => {
     getValues,
     formState: { errors },
     setError,
+    clearErrors,
   } = useForm({
     mode: "onChange",
   });
@@ -98,6 +99,9 @@ export const Search = () => {
             <Input
               {...register("search", {
                 required: "내용은 필수 입니다 ~_~",
+                onChange() {
+                  clearErrors("result");
+                },
               })}
               type="text"
               placeholder="영화 검색..."
